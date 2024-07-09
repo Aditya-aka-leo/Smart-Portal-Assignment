dd1a9089631689ef3e751120b0204887c547c207
Aditya-aka-leo <leohaxter@gmail.com>
2024-07-09 20:07:51 +0530

    dashboard

--- client/server.js
+++ client/server.js
@@ -11,16 +11,23 @@ app.use(express.static('public'));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const clients = new Map();
+
 wss.on('connection', (ws, request) => {
   const user_id = new URL(request.url, `http://${request.headers.host}`).searchParams.get('userId');
+  clients.set(user_id, ws);
 
   console.log(`User ${user_id} connected`);
 
   notifySystem(user_id, 'online');
 
-  ws.on('close', () => {
-    console.log(`User ${user_id} disconnected`);
+  ws.on('message', (message) => {
+    console.log(`Received message from user ${user_id}: ${message}`);
+  });
 
+  ws.on('close', () => {
+    clients.delete(user_id);
+    console.log(`User ${user_id} disconnected`);
     notifySystem(user_id, 'offline');
   });
 
@@ -28,11 +35,6 @@ wss.on('connection', (ws, request) => {
     console.error(`WebSocket error for user ${user_id}:`, error);
   });
 });
-
-server.listen(port, () => {
-  console.log(`Server running at http://:${port}`);
-});
-
 const notifySystem = async (user_id, status) => {
   try {
     const payload = {
@@ -51,3 +53,23 @@ const notifySystem = async (user_id, status) => {
     console.error('Error sending notification:', error.message);
   }
 };
+
+const sendNotification = (user_id, message) => {
+  const client = clients.get(user_id);
+  if (client && client.readyState === WebSocket.OPEN) {
+    client.send(message);
+  }
+};
+
+
+app.post('/sendNotification', express.json(), (req, res) => {
+  const { user_id, message } = req.body;
+  sendNotification(user_id, message);
+  res.sendStatus(200);
+});
+
+
+server.listen(port, () => {
+  console.log(`Server running at http://:${port}`);
+});  
+
--- kafka/consumer/controller/SendPushNotification.js
+++ kafka/consumer/controller/SendPushNotification.js
@@ -1,8 +1,25 @@
 const { produceMessage } = require("../utils/producer");
+const axios = require('axios');
 
 const SendPushNotification = async (msg) => {
   try{ 
     console.log("trying to send push notification");
+
+    data = {
+      user_id: msg.user_id,
+      message: msg.message
+    }
+
+    await axios.post(`http://20.244.93.34:3000/sendNotification`, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(`Notification sent to user ${user_id}: ${message}`);
+
+
+
+
   }catch(err){
      try {
           await produceMessage("Replay-Failed-Notification", msg);
