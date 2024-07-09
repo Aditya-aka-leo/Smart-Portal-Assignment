780759a0cb23ed839e38b35641aef56e84a311fb
Aditya-aka-leo <leohaxter@gmail.com>
2024-07-09 13:01:54 +0530

    Making a prod branch

--- server/Server.js
+++ server/Server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", main_route);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.get("*", (req, res) => {
